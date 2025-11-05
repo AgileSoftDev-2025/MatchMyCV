1e08fd3980bb757144d2422b4e0b914c903cec9b
virgieee <virseptia@gmail.com>
2025-11-05 15:14:28 +0700

    debugging

--- matchmycv_website/cv_analyzer/views.py
+++ matchmycv_website/cv_analyzer/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def analisis_cv(request):
-    return render(request, 'cv_analyzer/analisis-cv.html')
+    return render(request, 'cv_analyzer/analisis_cv.html')
 
 def hasil_rekomendasi(request):
     return render(request, 'cv_analyzer/hasil_rekomendasi.html')
